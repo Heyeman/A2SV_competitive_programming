edf513e99dfe281f8ceea359bc756ce5927ba108
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-03-09 16:00:43 +0300

    Create recentCalls.py

---
+++ week6/recentCalls.py
@@ -0,0 +1,16 @@
+class RecentCounter:
+    from collections import deque
+    def __init__(self):
+        self.queue = deque(list())        
+
+    def ping(self, t: int) -> int:
+        #print(self.queue)
+        while self.queue and self.queue[0] < t - 3000:
+            self.queue.popleft()
+        self.queue.append(t)
+        return len(self.queue)
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
