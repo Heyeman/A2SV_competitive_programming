508d6cda7c0d04cbdcc30f899856b56d9dec34d8
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-03-16 15:42:07 +0300

    Create mergeLists.py

---
+++ week7/mergeLists.py
@@ -0,0 +1,44 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = None
+        if list1 and list2:
+            if list1.val <= list2.val:
+                head = list1
+                list1 = list1.next
+            else:
+                head = list2
+                list2 = list2.next
+        elif not list1 and not list2:
+            head = None
+        elif not list1:
+            head = list2
+            list2 = list2.next
+        elif not list2:
+            head = list1
+            list1 = list1.next
+        pre = head
+        while head:
+            if list1 and list2:
+                if list1.val <= list2.val:
+                    head.next = list1
+                    list1 = list1.next
+                else:
+                    head.next = list2
+                    list2 = list2.next
+            elif list1:
+                head.next = list1
+                list1 = list1.next
+            elif list2:
+                head.next = list2
+                list2 = list2.next
+            else:
+                head.next = None
+            head = head.next
+        return pre
+            
+            
