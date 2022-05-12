847eddb0c9ae8980c518dc9bd3a0c6e0bbc0a7ec
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-05-12 20:14:47 +0300

    Create BTpreorderTraversal.py

---
+++ camp/graph/tree/BTpreorderTraversal.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        def values(root):
+            if not root:
+                return []
+            res = []
+            res.append(root.val)
+            if root.left:
+                res.extend(values(root.left))
+            if root.right:
+                res.extend(values(root.right))
+            return res
+        return values(root)
