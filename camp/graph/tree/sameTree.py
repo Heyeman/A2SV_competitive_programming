749eff08401c05c66e13890ad7b264e8d5ccfd21
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-05-12 18:20:09 +0300

    Create sameTree.py

---
+++ camp/graph/tree/sameTree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def bfs(node):
+            q = deque([node])
+            # print("q", q)
+            vals = []
+            while q:
+                curr = q.popleft()
+                if curr:
+                    vals.append(curr.val)
+                    q.append(curr.left)
+                    q.append(curr.right)
+                else:
+                    vals.append(None)
+
+            return vals
+                
+                
+            
+
+            
+            
+        
+        visited = set()
+        l1, l2 = [], []
+        l1  = bfs(p)
+        l2 = bfs(q)
+        return l1 == l2
+        
