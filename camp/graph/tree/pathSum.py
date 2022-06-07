52ae69d21857cb7a136794bf21d85bd042483276
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-06-07 16:17:56 +0300

    Update pathSum.py

--- camp/graph/tree/pathSum.py
+++ camp/graph/tree/pathSum.py
@@ -6,22 +6,17 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        def dfs(root, sums):
-            nonlocal res
-            if root and root.left:
-                dfs(root.left, sums+root.val)
-            if root and root.right:
-                dfs(root.right, sums+root.val)
-            if root and not root.left and not root.right and sums+root.val == targetSum:
-                res = True
+        self.res = False
+        def levelOrder(node, lvlSum):
+            if not node:
+                return 0
+            curr = lvlSum + node.val
+            if node.left:
+                levelOrder(node.left, curr)
+            if node.right:
+                levelOrder(node.right, curr)
+            if not node.left and not node.right and not self.res:
+                self.res = curr == targetSum
+        levelOrder(root, 0)
+        return self.res
                 
-        
-        
-        
-        
-        
-        res = False
-        dfs(root, 0)
-        return res
-            
-        
